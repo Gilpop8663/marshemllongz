1129b86d561f7b3dc1cf549cb860c904191c40e8
gilpop8663 <wolfye@naver.com>
2023-06-14 16:08:44 +0900

    chore: recoil, storybook 설치 및 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+};
+
+export default preview;
