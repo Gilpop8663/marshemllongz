3bdad3aa1c6177f59786dc361e2fe95439ea640a
gilpop8663 <wolfye@naver.com>
2023-06-18 16:37:34 +0900

    feat: 상품 상세 페이지의 장바구니 리스트 컴포넌트 구현 및 스토리북 적용

---
+++ components/shop/ProductDetail/ProductCartList.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MOCK_PRODUCT_CART } from '@constants/mockData';
+import ProductCartList from './ProductCartList';
+
+const meta: Meta<typeof ProductCartList> = { component: ProductCartList };
+
+export default meta;
+
+type Story = StoryObj<typeof ProductCartList>;
+
+export const Default: Story = {
+  args: {
+    productList: MOCK_PRODUCT_CART,
+  },
+};
---
+++ components/shop/ProductDetail/ProductCartList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ProductCart } from '@type/productType';
+import ProductCartItem from './ProductCartItem';
+
+interface ProductCartListProps {
+  productList: ProductCart[];
+}
+
+export default function ProductCartList({ productList }: ProductCartListProps) {
+  return (
+    <div className="flex flex-col gap-8">
+      {productList.map((product) => (
+        <ProductCartItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
