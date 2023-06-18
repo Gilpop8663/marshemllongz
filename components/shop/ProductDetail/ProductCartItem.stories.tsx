b452d530288b1516cbc58dbaf6eaa3e9f9bba7a9
gilpop8663 <wolfye@naver.com>
2023-06-18 16:37:19 +0900

    feat: 상품 상세 페이지의 장바구니 아이템 컴포넌트 구현 및 스토리북 적용

---
+++ components/shop/ProductDetail/ProductCartItem.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MOCK_PRODUCT_CART } from '@constants/mockData';
+import ProductCartItem from './ProductCartItem';
+
+const meta: Meta<typeof ProductCartItem> = { component: ProductCartItem };
+
+export default meta;
+
+type Story = StoryObj<typeof ProductCartItem>;
+
+export const Default: Story = {
+  args: {
+    product: MOCK_PRODUCT_CART[0],
+  },
+};
---
+++ components/shop/ProductDetail/ProductCartItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { getFormattedPrice } from '@utils/common';
+import { ProductCart } from '@type/productType';
+import QuantitySelector from '../QuantitySelector';
+
+interface ProductCartItemProps {
+  product: ProductCart;
+}
+
+export default function ProductCartItem({ product }: ProductCartItemProps) {
+  const { price, quantity, title } = product;
+
+  return (
+    <div className="flex justify-between h-14 sm:h-16">
+      <div className="flex flex-col justify-between">
+        <span className="text-xs sm:text-sm">{title}</span>
+        <QuantitySelector
+          quantity={quantity}
+          ariaDecreaseLabel={`${title} 수량 감소`}
+          ariaIncreaseLabel={`${title} 수량 증가`}
+          handleDecreaseClick={() => {}}
+          handleIncreaseClick={() => {}}
+        />
+      </div>
+      <div className="flex flex-col justify-between items-end ">
+        <button aria-label={`${title} 상품 삭제`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4 sm:w-5 sm:h-5"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+        <span className="text-xs sm:text-sm">{getFormattedPrice(price)}원</span>
+      </div>
+    </div>
+  );
+}
