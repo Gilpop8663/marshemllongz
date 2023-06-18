1a97554646e277298cd81376734e7725faf599c8
gilpop8663 <wolfye@naver.com>
2023-06-18 16:38:57 +0900

    styles: 셀렉트 컴포넌트 옵션창이 열렸을 때 absolute로 열리도록 변경

--- components/common/Select/index.tsx
+++ components/common/Select/index.tsx
@@ -6,7 +6,7 @@ interface OptionItemsProps {
   description: string;
 }
 
-interface SelectProps {
+export interface SelectProps {
   title: string;
   selectedItemLabel: string;
   optionItems: OptionItemsProps[];
@@ -32,42 +32,46 @@ export default function Select({
 
   return (
     <div className="flex flex-col">
-      <label className="text-xs mb-2" htmlFor={title + optionItems[0].label}>
-        {title}
-      </label>
-      <div
-        className="flex justify-between items-center border border-primaryBlack px-3 py-2 text-sm cursor-pointer font-sans "
-        id={title + optionItems[0].label}
-        onClick={toggleOpen}
-      >
-        <span>{selectedItemLabel}</span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-3 h-3"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-          </svg>
-        </span>
-      </div>
-      {isOpen && (
-        <div className="border border-t-0 border-primaryBlack cursor-pointer font-sans">
-          {optionItems.map((optionItem) => (
-            <div
-              className="flex justify-between px-3 py-2 text-sm hover:bg-whiteGray"
-              key={optionItem.label}
-              onClick={onSelectClick}
+      <div className="relative">
+        <label className="text-xs mb-2" htmlFor={title + optionItems[0].label}>
+          {title}
+        </label>
+        <div
+          className="flex justify-between items-center border border-primaryBlack px-3 py-2 text-sm cursor-pointer font-sans "
+          id={title + optionItems[0].label}
+          onClick={toggleOpen}
+        >
+          <span>{selectedItemLabel}</span>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-3 h-3"
             >
-              <span>{optionItem.label}</span>
-              <span>{optionItem.description}</span>
-            </div>
-          ))}
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+          </span>
         </div>
-      )}
+        {isOpen && (
+          <div className="relative">
+            <div className="absolute z-10 top-0 w-full bg-primaryWhite  border border-t-0 border-primaryBlack cursor-pointer font-sans">
+              {optionItems.map((optionItem) => (
+                <div
+                  className="flex justify-between px-3 py-2 text-sm hover:bg-whiteGray"
+                  key={optionItem.label}
+                  onClick={onSelectClick}
+                >
+                  <span>{optionItem.label}</span>
+                  <span>{optionItem.description}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
