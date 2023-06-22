e023b1d1fd9d11f43334fa63e70fa1c8cad410b4
gilpop8663 <wolfye@naver.com>
2023-06-22 21:19:37 +0900

    feat: 헤더 카테고리 컴포넌트 구현 및 스토리북 적용

---
+++ components/layout/Header/HeaderCategory.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { getClassNames } from '@utils/common';
+
+interface HeaderCategoryProps {
+  title: string;
+  category: string[];
+}
+
+export default function HeaderCategory({ title, category }: HeaderCategoryProps) {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <div
+      className="relative text-primaryBlack"
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
+    >
+      <div className="text-xs font-medium cursor-pointer hover:text-darkGray">{title}</div>
+      <div
+        className={getClassNames(
+          isHover ? 'opacity-100' : 'opacity-0',
+          'flex flex-col absolute bg-white border p-2 text-xs gap-2 transition-opacity font-medium top-6 -left-2'
+        )}
+      >
+        {category.map((item) => (
+          <span key={item} className="hover:text-darkGray cursor-pointer">
+            {item}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
