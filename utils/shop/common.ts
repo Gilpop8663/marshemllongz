59a550ccf2020ae27fed34aed2bc9262fe8d9607
gilpop8663 <wolfye@naver.com>
2023-06-25 16:06:08 +0900

    feat: 할인된 금액을 반환하는 함수 구현 및 테스트 작성

--- utils/shop/common.ts
+++ utils/shop/common.ts
@@ -1,3 +1,5 @@
+import { getPercentageNumber } from '@utils/common';
+
 interface GetPageRangeParams {
   totalPages: number;
   currentPage: number;
@@ -8,4 +10,21 @@ export const getPageRange = ({ currentPage, totalPages }: GetPageRangeParams) =>
   const endPage = Math.min(startPage + 4, totalPages);
 
   return [startPage, endPage];
+};
+
+interface GetDiscountedPriceParams {
+  discountPercent: number | undefined;
+  price: number;
+}
+
+export const getDiscountedPrice = ({ discountPercent, price }: GetDiscountedPriceParams) => {
+  if (!discountPercent) return null;
+
+  const discountPrice = getPercentageNumber({ total: price, percent: discountPercent });
+
+  if (!discountPrice) return null;
+
+  const discountedPrice = price - Math.round(discountPrice);
+
+  return discountedPrice;
 };
