47bbe193ff058bd211e7baf5201fe816672dc0b7
Gilpop8663 <wolfye@naver.com>
2023-06-30 12:51:47 +0900

    feat: 상품 상세 설명 컴포넌트 구현 및 스토리북 적용

---
+++ components/shop/ProductContent/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface ProductContentProps {
+  title: string;
+  contentList: string[];
+}
+
+export default function ProductContent({ title, contentList }: ProductContentProps) {
+  const tags = contentList.map((content, index) => {
+    if (content.slice(0, 4) === 'http') {
+      return (
+        <div key={content + index} className="relative w-96 aspect-square">
+          <Image src={content} fill alt={`${title} 사진`} className="object-cover" />
+        </div>
+      );
+    }
+
+    return <p key={content + index}>{content}</p>;
+  });
+
+  return <div className="whitespace-pre-wrap">{tags}</div>;
+}
